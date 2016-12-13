2cc408187d6d76793920fc26086a936f5157b5ec
Jianzhengzhou <water.zhou@yandex.com>
2016-12-13 15:59:50 +0800

    fix some bug and add support to RANLINK 2870 usb wifi
    Signed-off-by: Jianzhengzhou <water.zhou@yandex.com>

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Tue Nov 29 13:03:54 CST 2016"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Dec 12 13:53:28 CST 2016"
 #define LINUX_COMPILE_BY "water"
 #define LINUX_COMPILE_HOST "water-K43SD"
 #define LINUX_COMPILER "gcc version 4.9.2 (Sourcery CodeBench Lite 2015.05-17) "
