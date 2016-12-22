108d4adcf9704016c664f799d7f1835daebf143a
Jianzhengzhou <water.zhou@yandex.com>
2016-12-22 16:16:36 +0800

    add adv7511 support ,dts and bitstream is not ready
    Signed-off-by: Jianzhengzhou <water.zhou@yandex.com>

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 7 */
+/* This file is auto generated, version 8 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#7 SMP PREEMPT Thu Dec 22 15:14:36 CST 2016"
+#define UTS_VERSION "#8 SMP PREEMPT Thu Dec 22 15:38:13 CST 2016"
 #define LINUX_COMPILE_BY "water"
 #define LINUX_COMPILE_HOST "water-K43SD"
 #define LINUX_COMPILER "gcc version 4.9.2 (Sourcery CodeBench Lite 2015.05-17) "
